eb433f967fa32aaa3d852be7d310d1dde48bef0b
tw0ten <twoten.eq@gmail.com>
2024-11-23 23:31:49 +0200

    +

--- mod.ts
+++ mod.ts
@@ -3,9 +3,9 @@ type s = string;
 
 export class Logger {
 	private s: s[];
-	private i;
+	private f;
 
-	constructor(n: s, i = (s: s[]) => {
+	constructor(n: s, f = (s: s[]) => {
 		const del = "|";
 		const scope = s.length === 0 ? "" : del + s.join(del);
 		const date = new Date();
@@ -13,18 +13,18 @@ export class Logger {
 			date.getMinutes().toString().padStart(2, "0")
 		}:${date.getSeconds().toString().padStart(2, "0")}${scope}]`;
 	}) {
-		this.i = i;
+		this.f = f;
 		this.s = [n];
 	}
 
-	push = (n: s) => this.s.push(n);
-	pop = () => this.s.pop();
+	n = (n: s) => this.s.push(n);
+	u = () => this.s.pop();
 
-	log = (...a: a) => console.log(this.i(this.s), ...a);
-	err = (...a: a) => console.error(this.i(this.s), ...a);
+	i = (...a: a) => console.log(this.f(this.s), ...a);
+	e = (...a: a) => console.error(this.f(this.s), ...a);
 }
 
 export default ((l) => {
-	l.pop();
+	l.u();
 	return l;
 })(new Logger(""));
